6fc0c68ae87ab9549290703d54a525d4aa950fc6
jordan-error <144554415+jordan-error@users.noreply.github.com>
2026-08-16 01:10:44 +0800

    Created new brightness check function

---
+++ task_a/brightness-Jordan.py
@@ -0,0 +1,76 @@
+import cv2
+import numpy as np
+
+def detect_brightness(video_path):
+    total_dark_pixels = 0
+    total_pixels = 0
+
+    # Open the video file and get number of frames
+    video = cv2.VideoCapture(video_path)
+    total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    for frame in range(0, total_frames, 30):
+        # Jump to the specific frame and read it
+        video.set(cv2.CAP_PROP_POS_FRAMES, frame) 
+        sucess, image = video.read()
+
+        if sucess:
+            # Convert the frame to grayscale and histogram
+            gray_scale_frame = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            histogram = cv2.calcHist([gray_scale_frame], [0], None, [256], [0, 256])
+
+            # Count pixels in dark range (0-80) and total pixels
+            dark_pixels = sum(histogram[i][0] for i in range(0, 81))
+            pixels = gray_scale_frame.shape[0] * gray_scale_frame.shape[1]
+
+            total_pixels += pixels
+            total_dark_pixels += dark_pixels
+    
+    darkness_ratio = total_dark_pixels/total_pixels
+
+    #Returns true if video is night
+    if darkness_ratio > 0.6:
+        print("Video is night")
+        adjust_brightness(video_path)
+    else:
+        print("Video is day")
+
+
+
+def adjust_brightness(video_path):
+     # Get video properties for output
+    fps = video.get(cv2.CAP_PROP_FPS)
+    width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    
+    # Create output video writer
+    output = cv2.VideoWriter("task_a/output",
+                          cv2.VideoWriter_fourcc(*'MJPG'),
+                          fps,
+                          (width, height))
+    
+    print("Adjusting brightness")
+    # Open the video file and get number of frames
+    video = cv2.VideoCapture(video_path)
+    total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    for frame in range(0, total_frames):
+        # Jump to the specific frame and read it
+        video.set(cv2.CAP_PROP_POS_FRAMES, frame) 
+        image = video.read()
+
+        #Add brightness
+        image_float = image.astype(np.float64)
+        brightened_image = image_float + 50
+        brightened_image = np.clip(brightened_image, 0, 255)
+        brightened_image = brightened_image.astype(np.uint8)
+
+        output.write(brightened_image)
+
+
+def run_brightness_process(video_path): 
+    print("Running brightness detection on video")
+    detect_brightness(video_path)
+
+
