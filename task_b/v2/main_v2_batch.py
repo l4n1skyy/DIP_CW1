c950b6a7d5d971b4c70ac5fd60ed1b573b9c449f
Tofu8278 <shifengtu617@gmail.com>
2026-08-19 16:47:27 +0800

    this should be the final debugging, sequence corrected

--- task_b/v2/main_v2_batch.py
+++ task_b/v2/main_v2_batch.py
@@ -6,21 +6,22 @@ import numpy as np
 import projection_v2
 
 
-#gets and reads the images
+#everything in as a main process
 def process_image(image_name):
     image_path = "task_b/v2/input/" + image_name + ".png"     #gets image from folder
-    image = cv2.imread(image_path)                  #read image
+    image = cv2.imread(image_path)                            #read image
 
     if image is None:
-        print("Error: Could not read", image_path)
+        print("Error: Could not read", image_path)  #error catching for empty / not valid images
         return
+    
     original = image.copy()                         #keeping the original image for paragraph cropping.
 
     gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)   #makes image grayscale
 
     #Finding otsu threshold
     #Otsu automatically finds the threshold value
-    otsu_value , _ = cv2.threshold(gray,0,255,cv2.THRESH_BINARY+ cv2.THRESH_OTSU)
+    otsu_value , otsu_image = cv2.threshold(gray,0,255,cv2.THRESH_BINARY+ cv2.THRESH_OTSU)
     print("Otsu threshold:", otsu_value)
 
     #the text becomes black = 0.
@@ -72,65 +73,58 @@ def process_image(image_name):
         original,
         paragraph_stats,
         saturation_threshold=15)
-    
+
+    #sorting the paragrapghs (ANOTHER SEQUENCE FIX AAA)
     sorted_stats = projection_v2.sort_paragraphs(paragraph_stats) #this sorts the paragrapgh from projection_v2.py
     detected = original.copy() #this prepares the detected image for display
+    #storage for sorted paragrapghs , a sequnce error was made when the output was out  
+    # becasue ioriginal images first and then paragrapgh instead of paragrapgh fisrt
+    paragraph_images = [] #array or list to temporarily stoer paragrapghs
+    sorted_stats = projection_v2.sort_paragraphs(paragraph_stats)
+    detected = original.copy()
+    #paragrapgh extraction
+    for paragraph_number, paragraph in enumerate(sorted_stats, start=1):
 
     #paragrapgh extraction , a loop is used to reduce redundant code
-    for paragraph_number, paragraph in enumerate(
+        for paragraph_number, paragraph in enumerate(
         sorted_stats,
         start=1):
 
-        #gets the position of each paragrapgh
-        x = paragraph[cv2.CC_STAT_LEFT]
-        y = paragraph[ cv2.CC_STAT_TOP]
-        #determines paragrapgh size
-        width = paragraph[cv2.CC_STAT_WIDTH]
-        height = paragraph[cv2.CC_STAT_HEIGHT]
+            #gets the position of each paragrapgh
+            x = paragraph[cv2.CC_STAT_LEFT]
+            y = paragraph[ cv2.CC_STAT_TOP]
+            #determines paragrapgh size
+            width = paragraph[cv2.CC_STAT_WIDTH]
+            height = paragraph[cv2.CC_STAT_HEIGHT]
 
-        #crops the paragrapgh size (GeeksForGeeks, 2026)
-        paragraph_image = original[
-            y:y + height,
-            x:x + width
-        ]
-        #Convert extracted paragraph to RGB.
-        paragraph_rgb = cv2.cvtColor(
-            paragraph_image,
-            cv2.COLOR_BGR2RGB
-        )
+            #crops the paragrapgh size (GeeksForGeeks, 2026)
+            paragraph_image = original[
+                y:y + height,
+                x:x + width
+            ]
 
-        #Display extracted paragraph.
-        projection_v2.show_image(
-            paragraph_rgb,
-            "Paragraph "
-            + str(paragraph_number)
-            + " - "
-            + image_name
-        )
+            #storage for paragrapgh for display later (sequence bug fix)
+            paragraph_images.append((paragraph_number, paragraph_image))
 
-        #this code is used to make the red outer bound rectangle
-        cv2.rectangle(
-            detected,
-            (
-                x,
-                y
-            ),
-            (
-                x + width,
-                y + height
-            ),
-            (
-                0,
-                0,
-                255
-            ),
-            3
-        )
+            #this code is used to make the red outer bound rectangle
+            cv2.rectangle(
+                detected,
+                (
+                    x,
+                    y
+                ),
+                (
+                    x + width,
+                    y + height
+                ),
+                (
+                    0,
+                    0,
+                    255
+                ),
+                3
+            )
     
-    #==============
-    #output segment
-    #==============
-
     #Convert original image from BGR to RGB for Matplotlib
     original_rgb = cv2.cvtColor(
         original,
@@ -142,30 +136,52 @@ def process_image(image_name):
         detected,
         cv2.COLOR_BGR2RGB
     )
-        #Original paper
+
+    #==============
+    #output segment
+    #==============
+
+    #Shows the original paper
     projection_v2.show_image(
             original_rgb,
             "Original Paper - " + image_name
         )
 
-
-    #Grayscale image
+    #Shows the grayscale image
     projection_v2.show_image(
         gray,
         "Grayscale Image - " + image_name,
         "gray"
     )
 
+    #Shows the otsu threshold image
+    projection_v2.show_image(otsu_image, "Otsu Threshold Image" + image_name)
 
-    #Binary image
+    #Shows the binary image
     projection_v2.show_image(
         binary,
         "Binary Image - " + image_name,
         "gray"
     )
 
+    #Displaying the histogram projections
+    #horizontal histogram
+    projection_v2.show_projection(
+        horizontal,
+        "Horizontal Histogram Projection - "
+        + image_name,
+        "Image Row"
+    )
 
-    #Horizontal and vertical table lines combined
+    #vertical histogram
+    projection_v2.show_projection(
+        vertical,
+        "Vertical Histogram Projection - "
+        + image_name,
+        "Image Column"
+    )
+
+    #Shows the detected horizontal and vertical table lines combined (if any , for all images)
     projection_v2.show_image(
         table_lines,
         "Detected Table Lines - " + image_name,
@@ -173,7 +189,7 @@ def process_image(image_name):
     )
 
 
-    #Complete table regions
+    #shows the table line regions (shows the whole area to be deleted / excluded)
     projection_v2.show_image(
         table_mask,
         "Detected Table Region - " + image_name,
@@ -181,7 +197,7 @@ def process_image(image_name):
     )
 
 
-    #Image after tables are removed
+    #shows the image after tables are removed
     projection_v2.show_image(
         clean_binary,
         "Image After Table Removal - " + image_name,
@@ -203,24 +219,23 @@ def process_image(image_name):
         "Detected Paragraphs - " + image_name
     )
 
+    #displays each paragragh (bug fix for sequence)
+    for paragraph_number,paragraph_image in paragraph_images:
 
-    # ========================================================
-    # STEP 24: DISPLAY HISTOGRAM PROJECTIONS
-    # ========================================================
+        #Convert extracted paragraph to RGB.
+        paragraph_rgb = cv2.cvtColor(
+            paragraph_image,
+            cv2.COLOR_BGR2RGB
+        )
 
-    projection_v2.show_projection(
-        horizontal,
-        "Horizontal Histogram Projection - "
-        + image_name,
-        "Image Row"
-    )
-
-    projection_v2.show_projection(
-        vertical,
-        "Vertical Histogram Projection - "
-        + image_name,
-        "Image Column"
-    )
+        #Display extracted paragraph.
+        projection_v2.show_image(
+            paragraph_rgb,
+            "Paragraph "
+            + str(paragraph_number)
+            + " - "
+            + image_name
+        )
 
 ###final output section
     print("Number of detected paragraphs:", len(sorted_stats))
